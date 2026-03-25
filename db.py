5bb61a8bb3b276a72646f6d9a93591b87e3bf5d0
hsh080204-svg <hsh080204@gmail.com>
2026-03-26 02:47:51 +0900

    Update db.py

--- db.py
+++ db.py
@@ -1,9 +1,4 @@
 import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-
-import mysql.connector
 import os
 
 def get_connection():
@@ -13,8 +8,4 @@ def get_connection():
         password=os.environ.get("MYSQLPASSWORD"),
         database=os.environ.get("MYSQLDATABASE"),
         port=os.environ.get("MYSQLPORT")
-    )
-        user="root",
-        password="12345678",
-        database="vocab_app"
     )
