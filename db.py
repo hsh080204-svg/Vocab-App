0f7fff271385bcaeee09cf7f5df90ee73e984ffd
hsh080204-svg <hsh080204@gmail.com>
2026-03-26 09:13:23 +0900

    Update db.py

--- db.py
+++ db.py
@@ -7,6 +7,6 @@ def get_connection():
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
         database=os.environ.get("MYSQLDATABASE"),
-        port=int(os.environ.get("MYSQLPORT", 3306))
+        port=int(os.environ.get("MYSQLPORT", 3306)),
         connection_timeout=5
     )
